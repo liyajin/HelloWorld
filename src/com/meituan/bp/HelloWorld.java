5241f2de17ddd5e143f9798a33f72505900a53e0
李雅琎 <liyajin@liyajindeMacBook-Pro.local>
2015-12-26 11:27:54 +0800

    Initial commit

---
+++ src/com/meituan/bp/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.meituan.bp;
+
+/**
+ * Created by liyajin on 15/12/25.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("liyajin, Hello World");
+    }
+}
